6faa129d5977be01db1aa3b23c56b4073c7bbda0
tilman <t136.git@gmail.com>
2024-10-08 22:58:37 +0200

    uncommented tst correction; formatting

--- src/alpha_energy.rs
+++ src/alpha_energy.rs
@@ -1,10 +1,10 @@
-use serde_json;
 use core::panic;
-use std::fs;
+use serde_json;
 use std::collections::HashMap;
 use std::fmt;
-use std::io::BufReader;
+use std::fs;
 use std::io::BufRead;
+use std::io::BufReader;
 
 use crate::CN;
 
@@ -52,25 +52,30 @@ impl Alphas {
         }
     }
 
-    pub fn new_from_json(file: String, atom_names: &mut HashMap<String, u8>  ) -> Alphas {
-
-
-        let mut alphas_input: [[[f64; 12]; super::NUM_ATOM_TYPES]; super::NUM_ATOM_TYPES] = [[[0_f64; 12]; super::NUM_ATOM_TYPES]; super::NUM_ATOM_TYPES];
-        let file = fs::File::open(file)
-            .expect("file should open read only");
-        let json: serde_json::Value = serde_json::from_reader(file)
-            .expect("file should be proper JSON");
+    pub fn new_from_json(file: String, atom_names: &mut HashMap<String, u8>) -> Alphas {
+        let mut alphas_input: [[[f64; 12]; super::NUM_ATOM_TYPES]; super::NUM_ATOM_TYPES] =
+            [[[0_f64; 12]; super::NUM_ATOM_TYPES]; super::NUM_ATOM_TYPES];
+        let file = fs::File::open(file).expect("file should open read only");
+        let json: serde_json::Value =
+            serde_json::from_reader(file).expect("file should be proper JSON");
 
         let mut unique_atom_index = 0;
         for key_value in json.as_object().expect("root should be object").iter() {
-            let diluted_in_atom_index: usize; 
+            let diluted_in_atom_index: usize;
             let atom_index: usize;
             let key_collection: Vec<&str> = key_value.0.split('_').collect::<Vec<&str>>();
             if key_collection[0] == "dilute" {
-                assert_eq!(key_collection.len(), 4, "wrong key in alphas json file"); 
+                assert_eq!(key_collection.len(), 4, "wrong key in alphas json file");
 
-                atom_index = *atom_names.get(key_collection[1]).expect("diluted energy key needs format 'diluted_Metall_on_Metall' ") as usize; 
-                diluted_in_atom_index =  *atom_names.get(key_collection[3]).ok_or(AlphaJsonError).expect("diluted energy key needs format 'diluted_Metall_on_Metall' ") as usize;
+                atom_index = *atom_names
+                    .get(key_collection[1])
+                    .expect("diluted energy key needs format 'diluted_Metall_on_Metall' ")
+                    as usize;
+                diluted_in_atom_index = *atom_names
+                    .get(key_collection[3])
+                    .ok_or(AlphaJsonError)
+                    .expect("diluted energy key needs format 'diluted_Metall_on_Metall' ")
+                    as usize;
             } else if key_collection[0] == "clean" {
                 atom_index = unique_atom_index;
                 diluted_in_atom_index = unique_atom_index;
@@ -79,32 +84,47 @@ impl Alphas {
                 panic!("bad key in alphas JSON file");
             }
 
-            assert!(key_value.1.as_object().ok_or(AlphaJsonError).expect("bad nested object").len()>= CN, "one of the energy lists has a wrong format");
+            assert!(
+                key_value
+                    .1
+                    .as_object()
+                    .ok_or(AlphaJsonError)
+                    .expect("bad nested object")
+                    .len()
+                    >= CN,
+                "one of the energy lists has a wrong format"
+            );
             let mut average_cn_12: f64 = 0.;
-            for (cn, energy) in key_value.1.as_object().ok_or(AlphaJsonError).expect("bad nested object").iter(){
-
-
+            for (cn, energy) in key_value
+                .1
+                .as_object()
+                .ok_or(AlphaJsonError)
+                .expect("bad nested object")
+                .iter()
+            {
                 let cn_key_collection: Vec<&str> = cn.split('-').collect::<Vec<&str>>();
                 if cn_key_collection.len() == 1 {
                     let cn_value = cn.parse::<usize>().expect("bad JSON");
-                    alphas_input[atom_index][diluted_in_atom_index][cn_value-1] = energy.as_f64().ok_or(AlphaEnergyValueError).expect("energy value is not a flaot");
+                    alphas_input[atom_index][diluted_in_atom_index][cn_value - 1] = energy
+                        .as_f64()
+                        .ok_or(AlphaEnergyValueError)
+                        .expect("energy value is not a flaot");
                 } else {
                     if cn_key_collection[0] != "12" {
                         panic!("no 12");
                     }
-                    average_cn_12 += energy.as_f64().ok_or(AlphaEnergyValueError).expect("energy value is not a flaot");
+                    average_cn_12 += energy
+                        .as_f64()
+                        .ok_or(AlphaEnergyValueError)
+                        .expect("energy value is not a flaot");
                 }
-
             }
-            alphas_input[atom_index][diluted_in_atom_index][11] = average_cn_12/3.;
+            alphas_input[atom_index][diluted_in_atom_index][11] = average_cn_12 / 3.;
 
             if key_collection[0] == "clean" {
                 unique_atom_index += 1;
             }
-
         }
-
-
 
         Alphas::new(alphas_input)
     }
@@ -165,18 +185,17 @@ impl Alphas {
         at_supp: u8,
         supp_ee: i64,
     ) -> f64 {
-
         if cn_metal == 0 {
             return 0.;
         }
 
         let mut energy = 0.;
 
-        if cn_metal == 3 {
-            energy += 0.3 ;
-        } else if cn_metal == 4 {
-            energy += 0.4 ;
-        }
+        // if cn_metal == 3 {
+        //     energy += 0.3 ;
+        // } else if cn_metal == 4 {
+        //     energy += 0.4 ;
+        // }
 
         // if cn_metal == 3 || cn_metal == 4 {
         //     return morse_potential(energy);
@@ -192,7 +211,6 @@ impl Alphas {
             });
 
         energy
-
     }
 
     pub fn e_one_atom<I>(
@@ -245,7 +263,6 @@ impl Alphas {
     }
 }
 
-
 #[derive(Clone, Debug)]
 pub struct NnData {
     pub cn_metal: u8,
@@ -267,22 +284,24 @@ fn morse_potential(e_well: f64) -> f64 {
     //          E       A           r_min
     // pd: 7.559e−20	1.576e＋10	2.907e−10
     // pt: 	1.136e−19	1.581e＋10	2.929e−10
-    
+
     // const D_NORMAL: f64 = 1./(2_f64.powf(0.5_f64));
     const D_CUBE_LEN: f64 = std::f64::consts::SQRT_2;
-     
+
     const A: f64 = 1.576e+10;
     const A_EV: f64 = 1.576e+10 / D_MIN;
     const D_MIN: f64 = 2.907e-10;
     const D_E: f64 = 7.559e-20;
     const E: f64 = std::f64::consts::E;
 
-
-    (-e_well * (1.-E.powf(-A * (-D_MIN * 0.15)).powi(2)) + e_well) 
+    (-e_well * (1. - E.powf(-A * (-D_MIN * 0.15)).powi(2)) + e_well)
 }
 
 ///returns alphas where alhpas[x][x] is pure atom_x and alhpas[x][y] is atom_x in atom_y
-pub fn read_alphas(alphas_file: String, atom_names: &mut HashMap<String, u8>) -> [[[f64; 12]; 2]; 2] {
+pub fn read_alphas(
+    alphas_file: String,
+    atom_names: &mut HashMap<String, u8>,
+) -> [[[f64; 12]; 2]; 2] {
     const LINE_COUNT: usize = 14;
 
     let path = std::path::Path::new(&alphas_file);
@@ -325,7 +344,6 @@ pub fn read_alphas(alphas_file: String, atom_names: &mut HashMap<String, u8>) ->
     alphas
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -358,8 +376,8 @@ mod tests {
 
     #[test]
     fn test_e_summed() {
-
-        let mut atom_names: std::collections::HashMap<String, u8> = std::collections::HashMap::new();
+        let mut atom_names: std::collections::HashMap<String, u8> =
+            std::collections::HashMap::new();
         let alpha_file: String = "./Pt_Pd.6.bat".to_string();
         let alphas_inp = super::read_alphas(alpha_file, &mut atom_names);
         // let alphas_inp = [alpha_pt, alpha_pd];
@@ -384,21 +402,21 @@ mod tests {
 
     #[test]
     fn test_show_alphas() {
-        let mut atom_names: std::collections::HashMap<String, u8> = std::collections::HashMap::new();
+        let mut atom_names: std::collections::HashMap<String, u8> =
+            std::collections::HashMap::new();
         let alpha_file: String = "./Pt_Pd.6.bat".to_string();
         let alphas_inp = super::read_alphas(alpha_file, &mut atom_names);
         let alphas = Alphas::new(alphas_inp);
 
-        struct TestPositin  {
-        cn_metal: u8,
-        nn_atom_type_count: [u8; super::super::NUM_ATOM_TYPES],
-        nn_nn_atom_type_count: [NnData],
+        struct TestPositin {
+            cn_metal: u8,
+            nn_atom_type_count: [u8; super::super::NUM_ATOM_TYPES],
+            nn_nn_atom_type_count: [NnData],
         }
 
-        
         let nn_nn_atom_bulk = NnData {
             cn_metal: 12,
-            nn_atom_type_count_num_list: [12,0],
+            nn_atom_type_count_num_list: [12, 0],
             atom_type: 0,
         };
 
@@ -407,33 +425,29 @@ mod tests {
             nn_nn_atom_type_count.push(nn_nn_atom_bulk.clone());
         }
 
-        let start_e = alphas.e_one_atom(11, [11,0], nn_nn_atom_type_count.into_iter(), 0, 0, 0);
-        let tst_e = alphas.e_one_atom_tst(4, [4,0], 0, 0, 0);
-        let barr = super::e_barrier(start_e-tst_e, start_e-tst_e);
-
-        
+        let start_e = alphas.e_one_atom(11, [11, 0], nn_nn_atom_type_count.into_iter(), 0, 0, 0);
+        let tst_e = alphas.e_one_atom_tst(4, [4, 0], 0, 0, 0);
+        let barr = super::e_barrier(start_e - tst_e, start_e - tst_e);
 
         let mut atom_0 = "";
-        for ( atom_name, index) in atom_names.iter(){
+        for (atom_name, index) in atom_names.iter() {
             if index == &0 {
-                atom_0 =  atom_name;
+                atom_0 = atom_name;
             }
-
         }
 
-
-        println!("distance {}", std::f64::consts::SQRT_2 / 2.  );
-        println!("morse pot {}", super::morse_potential(barr) );
+        println!("distance {}", std::f64::consts::SQRT_2 / 2.);
+        println!("morse pot {}", super::morse_potential(barr));
         println!("move in bulk of {}: {}", atom_0, barr);
-
     }
 
     #[test]
-    fn test_alphas_from_json(){
+    fn test_alphas_from_json() {
         let file_path = "./alpha_alt.json".to_string();
-        let mut atom_names: std::collections::HashMap<String, u8> = std::collections::HashMap::new();
-        let mut old_atom_names: std::collections::HashMap<String, u8> = std::collections::HashMap::new();
-
+        let mut atom_names: std::collections::HashMap<String, u8> =
+            std::collections::HashMap::new();
+        let mut old_atom_names: std::collections::HashMap<String, u8> =
+            std::collections::HashMap::new();
 
         let alphas = Alphas::new_from_json(file_path, &mut atom_names);
 
@@ -441,22 +455,21 @@ mod tests {
         let alphas_inp = super::read_alphas(old_alpha_file, &mut old_atom_names);
         let old_alphas = Alphas::new(alphas_inp);
 
-
         println!("old atom_names: {:?}", old_atom_names);
         println!("new atom_names: {:?}", atom_names);
 
-        for atom_name in  atom_names.keys() {
-            for in_atom_name in  atom_names.keys() {
-                    for cn_i in 0..CN {
-                        println!("{}", alphas.cn[*atom_names.get(atom_name).unwrap() as usize][*atom_names.get(in_atom_name).unwrap() as usize][cn_i]);
-                        // assert_eq!(alphas.cn[*atom_names.get(atom_name).unwrap() as usize][*atom_names.get(in_atom_name).unwrap() as usize][cn_i]
-                        //     , old_alphas.cn[*old_atom_names.get(atom_name).unwrap() as usize][*old_atom_names.get(in_atom_name).unwrap() as usize][cn_i]);
-                    }
+        for atom_name in atom_names.keys() {
+            for in_atom_name in atom_names.keys() {
+                for cn_i in 0..CN {
+                    println!(
+                        "{}",
+                        alphas.cn[*atom_names.get(atom_name).unwrap() as usize]
+                            [*atom_names.get(in_atom_name).unwrap() as usize][cn_i]
+                    );
+                    // assert_eq!(alphas.cn[*atom_names.get(atom_name).unwrap() as usize][*atom_names.get(in_atom_name).unwrap() as usize][cn_i]
+                    //     , old_alphas.cn[*old_atom_names.get(atom_name).unwrap() as usize][*old_atom_names.get(in_atom_name).unwrap() as usize][cn_i]);
+                }
             }
         }
-
-
     }
-
-    
 }
